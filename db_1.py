6ac7fac1659cb154671a88a766b3143fb89eb3ca
Oleg050375 <ov_varganov@mail.ru>
2024-12-02 06:27:24 +0500

    Add files via upload

---
+++ db_1.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')  # создание соединения с БД
+
+cursor = connection.cursor()  # создание курсора
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+ id INTEGER PRIMARY KEY,
+ username TEXT NOT NULL,
+ email TEXT NOT NULL,
+ age INTEGER,
+ balance INTEGER NOT NULL)''')  # создание БД
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users(email)')  # создание индекса
+
+for i in range(1, 10):  # наполнение БД
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)',
+                   (f'User{i}', f'example{i}@gmail.com', 10 * i, 1000))
+
+for i in [1, 3, 5, 7, 9]:  # изменение значений полей в БД
+    cursor.execute('UPDATE Users SET balance = ? WHERE username=?',
+                   (500, f'User{i}'))
+
+for i in [1, 4, 7, 10]:  # удаление записей из БД по заданному параметру
+    cursor.execute('DELETE FROM Users WHERE username = ?',
+                   (f'User{i}',))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?',
+               (60,))  # выборка из БД по заданным параметрам
+for i in cursor.fetchall():  # вывод результатов выборки из БД
+    print(f'Имя:{i[0]} | Почта:{i[1]} | Возраст:{i[2]} | Баланс:{i[3]}')
+
+cursor.execute('DELETE FROM Users WHERE id = ?',
+               (6,))
+
+cursor.execute('SELECT COUNT(*) FROM Users')  # запрос на подсчёт кол-ва записей в БД
+us_cnt = cursor.fetchone()[0]  # сохранение результата запроса в переменную
+
+cursor.execute('SELECT SUM(balance) FROM Users')  # запрос на суммирование всех балансов
+bal_sum = cursor.fetchone()[0]  # сохранение результата запроса в переменную
+
+print(bal_sum / us_cnt)  # вычисление и печать среднего значения баланса
+
+connection.commit()  # сохранение изменений в БД
+
+connection.close()  # закрытие соединения с БД
