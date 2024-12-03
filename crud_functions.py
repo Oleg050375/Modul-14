44d1b0ac235d4214dd7fe5447c8a6340c0fb80a0
Oleg050375 <ov_varganov@mail.ru>
2024-12-03 22:47:31 +0500

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+connection = sqlite3.connect('Products.db')  # создание соединения с БД
+
+cursor = connection.cursor()  # создание курсора
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Products(
+     id INTEGER PRIMARY KEY,
+     title TEXT NOT NULL,
+     description TEXT NOT NULL,
+     picture TEXT NOT NULL,
+     price INTEGER NOT NULL)''')  # создание БД
+
+
+def initiate_db(user_id, title, description, picture, price):  # функция наполнения БД
+    cursor.execute('INSERT INTO Products(id,title,description,picture,price) VALUES(?,?,?,?,?)',
+                   (f'{user_id}', f'{title}', f'{description}', f'{picture}', f'{price}'))  # занесение в БД
+
+
+def get_all_products():  # функция получения всех записей БД
+    cursor.execute('SELECT * FROM Products')
+    pr_list = cursor.fetchall()
+    return pr_list
+
+#connection.commit()  # сохранение изменений в БД
+
+#connection.close()  # закрытие соединения с БД
